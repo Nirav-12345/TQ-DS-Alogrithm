badbcea66ed71227f7d0f2ffd6a5d954afc64873
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-10 05:09:12 -0800

    adding maual file

---
+++ Palindrome-Prime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PalindromePrime;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int flag = 0;
+
+        for (int n = 0; n <= 1000; n++)
+        {
+            flag = 0;
+
+            for (int i = 2; i <= n / 2; i++)
+
+            {
+                if (n % i == 0)
+
+
+
+                {
+                    flag = 1;
+
+
+                }
+
+
+            }
+
+            if (flag == 0)
+            {
+                Console.WriteLine("prime" + n);
+            }
+
+            if (n % 11 == 0)
+            {
+                Console.WriteLine("Palindrome" + n);
+            }
+        }
+
+        
+
+    }
+}
