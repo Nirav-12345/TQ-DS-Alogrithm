d8cd4f43c1445ec8d12ecbc610d66b09ff5686fc
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-10 05:03:25 -0800

    adding Manual file

---
+++ Anagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Anagram;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        string str1 = "earth";
+        string str2 = "heart";
+
+        char[] ch1 = str1.ToLower().ToCharArray();
+
+        char[] ch2 = str2.ToLower().ToCharArray();
+
+        Array.Sort(ch1);
+        Array.Sort(ch2);
+        string val1 = new string(ch1);
+        string val2 = new string(ch2);
+
+        if (val1==val2)
+        {
+            Console.WriteLine("Anagram");
+        }
+
+        else { Console.WriteLine("not Anagram"); }
+
+    }
+}
